0d21a9aa5e4026b6fb7c3608b6193cd5196109bf
Christoph Stach <christoph.stach@gmail.com>
2021-02-04 00:54:48 +0800

    Add highlight.js

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,6 +3,6 @@ import html from 'remark-html'
 import highlight from 'remark-highlight.js'
 
 export default function markdownToHtml(markdown: string): string {
-  const result = remark().use(highlight).use(html).processSync(markdown)
+  const result = remark().use(html).use(highlight).processSync(markdown)
   return result.toString()
 }
